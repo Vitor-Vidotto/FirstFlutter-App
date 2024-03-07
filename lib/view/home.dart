beac0708956f153359a8035ed528c5f6d791f68a
vitor-vidotto <vitotbrbr@gmail.com>
2024-03-07 16:29:32 -0300

    chore: update in the buttons

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/view/menu.dart';
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -10,43 +10,72 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color.fromRGBO(255, 255, 255, 1),
         title: Text(title),
       ),
-      body: Center(child: Column(children: [
-        const Padding(
-            padding: EdgeInsets.all(20.0),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'User',
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: NetworkImage(
+              "https://img.freepik.com/vetores-gratis/fundo-azul-geometrico-vetor-de-papel-de-parede-da-area-de-trabalho_53876-135927.jpg"),
+          fit: BoxFit.cover,
+        )),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(20.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelStyle:
+                        TextStyle(color: Color.fromRGBO(222, 25, 80, 100)),
+                    labelText: 'User',
+                  ),
+                  style: TextStyle(color: Color.fromRGBO(222, 25, 80, 100)),
+                ),
               ),
-            )),
-        const Padding(
-            padding: EdgeInsets.all(20.0),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
+              const Padding(
+                padding: EdgeInsets.all(20.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelStyle:
+                        TextStyle(color: Color.fromRGBO(222, 25, 80, 100)),
+                    labelText: 'Password',
+                  ),
+                  style: TextStyle(color: Color.fromRGBO(222, 25, 80, 100)),
+                ),
               ),
-            )),
-        Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: TextButton(
-            style: TextButton.styleFrom(
-              foregroundColor:  Color.fromARGB(255, 13, 145, 101),
-            ),
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        const RastrearPedidoPage(title: "Rastreador do Pedido"),
-                  ));
-            },
-            child: Text('Entrar'),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: GestureDetector(
+                  child: Container( padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(0, 215, 200, 100),
+                      border:
+                          Border.all(color: Color.fromRGBO(255, 255, 255, 1)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text(
+                      "Entrar",
+                      style: TextStyle(color: Color.fromRGBO(255, 0, 0, 100)),
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const RastrearPedidoPage(
+                                title: "Rastreador do Pedido")));
+                  },
+                ),
+              ),
+            ],
           ),
         ),
-      ]),
-    ),);
+      ),
+    );
   }
 }
--- lib/view/menu.dart
+++ lib/view/menu.dart
@@ -1,9 +1,10 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_application_1/view/pesquisa.dart';
 
 class RastrearPedidoPage extends StatelessWidget {
-  const RastrearPedidoPage({super.key, required this.title});
+  const RastrearPedidoPage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -13,36 +14,70 @@ class RastrearPedidoPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(this.title),
       ),
-      body:
-       Column(
-        children: [
-          const Padding(padding: EdgeInsets.all(20.0), child: TextField(
-               decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Numero do Pedido',
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+                "https://img.freepik.com/vetores-gratis/fundo-azul-geometrico-vetor-de-papel-de-parede-da-area-de-trabalho_53876-135927.jpg"), // Substitua pelo URL da sua imagem
+            fit: BoxFit.cover,
+          ),
         ),
-          )),
-        const Padding(padding: EdgeInsets.all(20.0), child: TextField(
-               decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Nome Destinatario',
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(20.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Numero do Pedido',
+                    labelStyle:
+                        TextStyle(color: Color.fromRGBO(222, 25, 80, 100)),
+                  ),
+                  style: TextStyle(color: Color.fromRGBO(222, 25, 80, 100)),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(20.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelStyle:
+                        TextStyle(color: Color.fromRGBO(222, 25, 80, 100)),
+                    labelText: 'Nome Destinatario',
+                  ),
+                  style: TextStyle(color: Color.fromRGBO(222, 25, 80, 100)),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: GestureDetector(
+                  child: Container( padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(0, 215, 200, 100),
+                      border:
+                          Border.all(color: Color.fromRGBO(255, 255, 255, 1)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text(
+                      "Buscar",
+                      style: TextStyle(color: Color.fromRGBO(255, 0, 0, 100)),
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>  const PedidoRastreado(title: "Pedidos")));
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
-          )),
-          Padding(padding: EdgeInsets.all(20.0),child: TextButton(style: TextButton.styleFrom(
-    foregroundColor: Color.fromARGB(255, 13, 145, 101),
-  ),
-  onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        const PedidoRastreado(title: "Pedidos"),
-                  ));
-            },
-            child: Text('Entrar'),
-          ),),
-        ]
-       ),
+      ),
     );
   }
 }
+
